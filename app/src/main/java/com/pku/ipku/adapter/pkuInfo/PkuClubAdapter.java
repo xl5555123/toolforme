e0731afaf73e48266bec1eebaea8fb02c611177a
phantom_pku <546477156@qq.com>
2015-01-09 14:22:18 +0800

    pkuclub demo

---
+++ app/src/main/java/com/pku/ipku/adapter/pkuInfo/PkuClubAdapter.java
@@ -0,0 +1,7 @@
+package com.pku.ipku.adapter.pkuInfo;
+
+/**
+ * Created by Allen on 2015/1/7.
+ */
+public class PkuClubAdapter {
+}
---
+++ app/src/main/java/com/pku/ipku/api/MockPkuClubActivityList.java
@@ -0,0 +1,7 @@
+package com.pku.ipku.api;
+
+/**
+ * Created by Allen on 2015/1/7.
+ */
+public class MockPkuClubActivityList {
+}
