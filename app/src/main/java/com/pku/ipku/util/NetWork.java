3ef3823beb2167753808cac1c18cbe6725c445a1
liuyi315 <liuyivector@gmail.com>
2015-01-22 15:51:01 +0800

    rk helper

---
+++ app/src/main/java/com/pku/ipku/util/NetWork.java
@@ -0,0 +1,37 @@
+package com.pku.ipku.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetWork {
+	//�ж��豸�Ƿ��Ѿ�����
+    public static boolean isNetworkAvailable( Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                if (connectivity == null) {
+                        return false;
+                } else {
+                        NetworkInfo info = connectivity.getActiveNetworkInfo();
+                        if(info == null){
+                                return false;
+                        }else{
+                                if(info.isAvailable()){
+                                        return true;
+                                }
+                        }
+                }
+        return false;
+    }
+    
+  //�ж��豸�Ƿ�����wifi
+    public static boolean isWifiAvailable( Context context) {
+    	ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+		if (mWifi.isConnected())
+		    return true;
+		else
+			return false;
+    }
+
+}
