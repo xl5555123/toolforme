c17367bbdb809a41b88cbf528bc2c9f7ceba6b45
xingliang <xl5555123@gmail.com>
2015-01-22 13:09:19 +0800

    save current work.

--- app/src/main/java/com/pku/ipku/ui/studyGuide/FreeClassroomFragment.java
+++ app/src/main/java/com/pku/ipku/ui/studyGuide/FreeClassroomFragment.java
@@ -8,7 +8,26 @@ import android.view.ViewGroup;
 
 import com.pku.ipku.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FreeClassroomFragment extends Fragment {
+
+    private List<String> buildings = new ArrayList<String>() {
+        {
+            add("第一教学楼");
+            add("第二教学楼");
+            add("第三教学楼");
+            add("第四教学楼");
+            add("文史");
+            add("电教");
+            add("理科教学楼");
+            add("化学");
+            add("电教听力");
+            add("国关");
+            add("政管");
+        }
+    };
 
     public FreeClassroomFragment() {
         // Required empty public constructor
