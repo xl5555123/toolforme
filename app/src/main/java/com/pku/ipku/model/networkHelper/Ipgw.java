1071350866ea1db443c5fa3fbd5398b659f1e7e7
xingliang <xl5555123@gmail.com>
2015-01-23 16:00:23 +0800

    bug-fix.

--- app/src/main/java/com/pku/ipku/model/networkHelper/Ipgw.java
+++ app/src/main/java/com/pku/ipku/model/networkHelper/Ipgw.java
@@ -1,5 +1,7 @@
 package com.pku.ipku.model.networkHelper;
 
+import com.pku.ipku.util.MyProtocolSocketFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
