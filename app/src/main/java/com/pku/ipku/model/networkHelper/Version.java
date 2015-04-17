1ce3889b3065960eeec656008e604dd832cd96bd
xingliang <xl5555123@gmail.com>
2015-04-17 09:26:31 +0800

    add version.

---
+++ app/src/main/java/com/pku/ipku/model/networkHelper/Version.java
@@ -0,0 +1,52 @@
+package com.pku.ipku.model.networkHelper;
+
+/**
+ * Created by XingLiang on 2015/4/16.
+ */
+public class Version {
+    private String name;
+    private int version;
+    private String changelog;
+    private String versionShort;
+    private String update_url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getChangelog() {
+        return changelog;
+    }
+
+    public void setChangelog(String changelog) {
+        this.changelog = changelog;
+    }
+
+    public String getVersionShort() {
+        return versionShort;
+    }
+
+    public void setVersionShort(String versionShort) {
+        this.versionShort = versionShort;
+    }
+
+    public String getUpdate_url() {
+        return update_url;
+    }
+
+    public void setUpdate_url(String update_url) {
+        this.update_url = update_url;
+    }
+}
